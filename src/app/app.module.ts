2f813f73db303487f79acda9deaf6df709f99ef7
Laravel Developer <prashant@neosofttech.com>
2020-01-30 10:54:27 +0530

    displayed simple page with contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyContainterComponent } from './myContainer/myContainer';
+import { TopnavComponent } from './myContainer/topnav/topnav.component';
+import { HeaderComponent } from './myContainer/header/header.component';
+import { Textsec1Component } from './myContainer/textsec1/textsec1.component';
+import { Textsec2Component } from './myContainer/textsec2/textsec2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainterComponent,
+    TopnavComponent,
+    HeaderComponent,
+    Textsec1Component,
+    Textsec2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/myContainer/myContainer.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-myContainter',
+    templateUrl: './myContainer.html',
+    styleUrls: ['./myContainer.css'],
+})
+ 
+export class MyContainterComponent implements OnInit {
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+}  
